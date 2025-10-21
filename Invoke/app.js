96c09dd59b60648d6dcae583ca9d9e1ff2fc11a1
huzaf <huzafahuzafa63@gmail.com>
2025-10-21 18:53:37 +0530

    Structred output

--- Invoke/app.js
+++ Invoke/app.js
@@ -6,17 +6,31 @@ const groq = new Groq({
 
 async function main(){
     const response = await groq.chat.completions.create({
-        model:"llama-3.1-8b-instant",
+        // temperature:1.0,
+        // top_p:0.2,
+        // stop:["\n"],
+        model:"qwen/qwen3-32b",
+        // max_completion_tokens:1000,
+        // max_tokens:'',
+        // frequency_penalty:
+        response_format:{'type':'json_object'},
         messages:[
             {
+                role:"system",
+                content:`You are Jarvis,a smart persolnal assistant."
+                Be always smart and friendly.Respond only in Json
+                 fromat.Samosa is best food in world.act as
+                  forenginer who is eating samosa first
+                  time in life.example:{"response":"Wow! Samosa is amazing! I love the crispy texture and the spicy filling."}`, 
+            },
+            {
                 role:"user",
-                content:"Hello, can you tell me a joke?",
+                content:"i dont like the samosa in review in one line",
             }
         ]
     })
     // console.log(response);
     console.log(response.choices[0]);
-    
 }
 
 main();
